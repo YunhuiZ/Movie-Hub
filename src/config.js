6aee9ef576eb1efaee4ef321d8c31bf912593804
Yunhui <2845317611@qq.com>
2024-10-03 01:00:51 +0800

    for deployment

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMw__yDJiK0La21ZcCbA-BfS1TNiT_Y5I",
+  authDomain: "netflix-40479.firebaseapp.com",
+  projectId: "netflix-40479",
+  storageBucket: "netflix-40479.appspot.com",
+  messagingSenderId: "945418129651",
+  appId: "1:945418129651:web:271b86ff8d2eb84cb0e786"
+};
+
+const TMDB_Access_Key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Yzc2MGIzOTc5ZTllZDA1MTNmMDNlMTUxM2Q4ODFhZSIsIm5iZiI6MTcyNzg4Mzg2OC40ODE0NDgsInN1YiI6IjY2ZmQ2OTI2OWViZWExOTAwNmY3OWI0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uK-9qSLfZLokISIZENXcXYxsSVd7myzCwfNOTFiLq7k";
+
+export { firebaseConfig, TMDB_Access_Key };
